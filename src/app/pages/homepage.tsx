950ae16ff45847ea60716ba3877b0b8924887134
Sebastian <sebastianbritezdevelop@gmail.com>
2024-08-21 10:11:24 -0400

    stats bar added

--- src/app/pages/homepage.tsx
+++ src/app/pages/homepage.tsx
@@ -3,12 +3,14 @@ import {NextUIProvider} from "@nextui-org/react";
 import AnimatedBg from "@/modules/animated_bg";
 import HeroSection from "../views/herosection";
 import Services from "../views/services";
+import Stats from "../views/stats";
 
 export default function HomePage() {
   return (
     <NextUIProvider>
         <HeroSection/>
         <AnimatedBg/>
+        <Stats/>
         <Services/>
         <HeroParallaxDemo/>
     </NextUIProvider>
---
+++ src/app/views/stats.tsx
@@ -0,0 +1,24 @@
+export default function Stats() {
+    return (
+        <section className=" w-auto flex flex-col justify-center items-center my-16">
+            <h1 className="text-3xl text-center w-3/5">Impulsa tu presencia en línea con nuestras soluciones integrales. Nos enfocamos en optimizar cada aspecto de tu negocio digital, desde el desarrollo hasta la gestión, asegurando un crecimiento constante y sostenible.</h1>
+
+            <div className="w-3/5 h-40 my-8 px-16 py-16 border border-black rounded-xl flex items-center justify-between">
+                <div className="flex space-x-2">
+                    <h1 className="text-7xl">+11</h1>
+                    <p className="text-2xl">Años de<br/>experiencia</p>
+                </div>
+                <div className="border-l border-black h-32"></div>
+                <div className="flex space-x-2">
+                    <h1 className="text-7xl">+150</h1>
+                    <p className="text-2xl">Clientes<br/>satisfechos</p>
+                </div>
+                <div className="border-l border-black h-32"></div>
+                <div className="flex space-x-2">
+                    <h1 className="text-7xl">+11</h1>
+                    <p className="text-2xl">Años de<br/>experiencia</p>
+                </div>
+            </div>
+        </section>
+    );
+}
